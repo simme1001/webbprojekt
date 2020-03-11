ac376aa99ea702dd5364e062acc2727ba07b306a
simme1001 <simon@feltenmark.com>
2020-03-11 14:42:33 +0100

    just kidding last one was not initial commit lmao

--- public/js/getlocation.js
+++ public/js/getlocation.js
@@ -1,11 +1,24 @@
+const request = require('request');
+
 module.exports = {
   getLocation: (req, res) => {
-    
+    let lat = 35;
+    let lon = 139;
+
+    /*
       if (navigator.geolocation) {
         console.log(navigator.geolocation.getCurrentPosition());
       }
       else {
         console.log("Geolocation is not supported by this browser.");
       }
+      */
+
+      request('https://samples.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon +  '&appid=b6907d289e10d714a6e88b30761fae22', function (error, response, body) {
+          console.log(JSON.parse(body, null, 4));
+      });
   }
+
+
+
 }
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const port = 3000;
 const app = express();
 
+// api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}
 
 //Länkar till modulen
 const {getLocation} = require('./public/js/getlocation.js')
@@ -12,8 +13,8 @@ const {getLocation} = require('./public/js/getlocation.js')
 console.log("The server is running on port: " + port + "...")
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
-  //res.send("Hello!");
-  res.render(path.join(__dirname, '/index.ejs'),{titel:"HELLO I AM HERE!"});
+  getLocation();
+  res.render(path.join(__dirname, '/index.ejs'));
 });
 
 app.listen(port);
