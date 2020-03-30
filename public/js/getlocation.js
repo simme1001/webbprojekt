83502865c3204f5a9e655db043a4b005d72b97b5
simme1001 <simon@feltenmark.com>
2020-03-31 00:36:43 +0200

    e

--- public/js/getlocation.js
+++ public/js/getlocation.js
@@ -1,24 +1,72 @@
 const request = require('request');
 
+function getLocation() {
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(showTemperature);
+  }
+  else{
+    console.log("Temperature Unavailable.");
+  }
+}
+
+function showTemperature(position){
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+}
+
 module.exports = {
-  getLocation: (req, res) => {
-    let lat = 35;
-    let lon = 139;
 
-    /*
-      if (navigator.geolocation) {
-        console.log(navigator.geolocation.getCurrentPosition());
-      }
-      else {
-        console.log("Geolocation is not supported by this browser.");
-      }
-      */
+getTemperature: (req, res) =>{
+  getLocation();
+  showTemperature(position);
+  request('api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon' + lon + '&appid=2b51b9dd07fa82fae66de057bfac1138', function (error, response, body){
+    console.log(JSON.parse(body, null, 4));
+    const newBody = JSON.parse(body);
+    let temp = newBody.main.temp;
+    temp -= 273;
+    document.getElementById("temperature").innerHTML = Math.round(temp);
+    console.log("Temp: " + Math.round(temp) + " °C");
+    })
+  }
+}
 
-      request('https://samples.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon +  '&appid=b6907d289e10d714a6e88b30761fae22', function (error, response, body) {
+/*
+  //getLocation: (req, res) => {
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+
+      request('api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude +'&lon' + position.coords.longitude + '&appid=2b51b9dd07fa82fae66de057bfac1138', function (error, response, body){
+      //request('https://samples.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon +  '&appid=b6907d289e10d714a6e88b30761fae22', function (error, response, body) {
           console.log(JSON.parse(body, null, 4));
+          const newBody = JSON.parse(body);
+          let temp = newBody.main.temp;
+          temp -= 273;
+          document.getElementById("temperature") = Math.round(temp);
+          console.log("Temp: " + Math.round(temp) + " °C");
       });
   }
+//}
 
 
 
+//This worked in index.ejs
+/*
+function getLocation(){
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(showPosition);
+  }
+  else{
+    document.getElementById("temperature").innerHTML = "Temperature Unavailable";
+  }
 }
+
+function showPosition(position){
+  document.getElementById("temperature").innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+}
+*/
+
+//const {getLocation} = require('./public/js/getlocation.js')
+
+//app.get('/getWeather', getLocation);
+
+//localhost:3000/getWeather
--- server.js
+++ server.js
@@ -8,13 +8,25 @@ const app = express();
 // api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}
 
 //Länkar till modulen
-const {getLocation} = require('./public/js/getlocation.js')
+//const {getTemperature} = require('./public/js/getlocation.js')
 
 console.log("The server is running on port: " + port + "...")
 app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
-  getLocation();
+  //getLocation();
+  //getTemperature();
   res.render(path.join(__dirname, '/index.ejs'));
+});
+
+app.get('/spel', function(req, res){
+  //getLocation();
+  res.render(path.join(__dirname, '/spel.ejs'));
+});
+
+app.get('/arbete', function(req, res){
+  //getLocation();
+  res.render(path.join(__dirname, '/arbete.ejs'));
 });
 
 app.listen(port);
